a22dd160780f38d924949b1ea0ede3d00c6719a9
Adam Kwan <adamk1230@gmail.com>
2019-10-03 22:22:48 -0400

    changed folder layout, added header component, started search component

---
+++ src/components/search/search.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+
+const Search = () => {
+  const [searchText, setSearchText] = useState(0);
+
+  return (
+    <div>
+      <input />
+    </div>
+  );
+};
+
+export default Search;
